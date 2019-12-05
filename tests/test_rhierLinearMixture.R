b041eb032d4df34c2af8213a3c290fd32f9ea8a8
Jingyu He <jingyuhe@hotmail.com>
2019-12-04 20:39:11 -0600

    update test code

--- tests/test_rhierLinearMixture.R
+++ tests/test_rhierLinearMixture.R
@@ -3,6 +3,8 @@ library(coda)
 library(clusterGeneration)
 
 R = 5000
+burnin = 3000
+
 # set.seed(66)
 nreg = 100
 nobs = 50
@@ -60,13 +62,16 @@ t3 = proc.time()
 out3 = rhierLinearMixture_slice(Data=Data1, Prior=Prior1, Mcmc=Mcmc1)
 t3 = proc.time() - t3
 
+# a second run of Gibbs
+out4 = rhierLinearMixture(Data=Data1, Prior=Prior1, Mcmc=Mcmc1)
+
 cat("Summary of Delta draws", fill=TRUE)
 summary(out1$Deltadraw, tvalues=as.vector(Delta))
 cat("Summary of Normal Mixture Distribution", fill=TRUE)
 summary(out1$nmix)
 ## plotting examples
 if(0){
-  plot(out1$betadraw)
+  plot(out1$betadraw[,,burnin:R])
   plot(out1$nmix)
   plot(out1$Deltadraw)
 }
@@ -76,36 +81,36 @@ if(0){
 
 draw = function(i, j){
     par(mfrow=c(3,3))
-    plot(out1$betadraw[i,j, 200:2000])
-    plot(out2$betadraw[i,j, 200:2000])
-    plot(out3$betadraw[i,j, 200:2000])
-    cat("gESS ", mean(out1$betadraw[i,j, 200:2000]), " ",  sd(out1$betadraw[i,j, 200:2000]), "\n")
-    cat("MH ", mean(out2$betadraw[i,j, 200:2000]), " ",  sd(out2$betadraw[i,j, 200:2000]), "\n")
-    cat("ESS ", mean(out3$betadraw[i,j, 200:2000]), " ",  sd(out2$betadraw[i,j, 200:2000]), "\n")
-    acf(out1$betadraw[i,j, 200:2000])
-    acf(out2$betadraw[i,j, 200:2000])
-    acf(out3$betadraw[i,j, 200:2000])
-    plot(density(out1$betadraw[i,j, 200:2000]))
-    lines(density(out2$betadraw[i,j, 200:2000]), col = "red")
-    lines(density(out3$betadraw[i,j, 200:2000]), col = "blue")
+    plot(out1$betadraw[,,burnin:R][i,j, burnin:R])
+    plot(out2$betadraw[,,burnin:R][i,j, burnin:R])
+    plot(out3$betadraw[,,burnin:R][i,j, burnin:R])
+    cat("gESS ", mean(out1$betadraw[,,burnin:R][i,j, burnin:R]), " ",  sd(out1$betadraw[,,burnin:R][i,j, burnin:R]), "\n")
+    cat("MH ", mean(out2$betadraw[,,burnin:R][i,j, burnin:R]), " ",  sd(out2$betadraw[,,burnin:R][i,j, burnin:R]), "\n")
+    cat("ESS ", mean(out3$betadraw[,,burnin:R][i,j, burnin:R]), " ",  sd(out2$betadraw[,,burnin:R][i,j, burnin:R]), "\n")
+    acf(out1$betadraw[,,burnin:R][i,j, burnin:R])
+    acf(out2$betadraw[,,burnin:R][i,j, burnin:R])
+    acf(out3$betadraw[,,burnin:R][i,j, burnin:R])
+    plot(density(out1$betadraw[,,burnin:R][i,j, burnin:R]))
+    lines(density(out2$betadraw[,,burnin:R][i,j, burnin:R]), col = "red")
+    lines(density(out3$betadraw[,,burnin:R][i,j, burnin:R]), col = "blue")
 }
 draw(1,1)
 
 
 
 # K-S test, whether two empirical distribution same or not
-p_values = matrix(0, dim(out1$betadraw)[1], dim(out1$betadraw)[2])
+p_values = matrix(0, dim(out1$betadraw[,,burnin:R])[1], dim(out1$betadraw[,,burnin:R])[2])
 
-for(i in 1:(dim(out1$betadraw)[1])){
-  for(j in 1:(dim(out1$betadraw)[2])){
-    p_values[i,j] = ks.test(out1$betadraw[i,j, ], out2$betadraw[i,j, ])$p.value
+for(i in 1:(dim(out1$betadraw[,,burnin:R])[1])){
+  for(j in 1:(dim(out1$betadraw[,,burnin:R])[2])){
+    p_values[i,j] = ks.test(out1$betadraw[,,burnin:R][i,j, ], out2$betadraw[,,burnin:R][i,j, ])$p.value
   }
 }
 
 
-mean(apply(out1$betadraw, c(1,2), effectiveSize))
-mean(apply(out2$betadraw, c(1,2), effectiveSize))
-mean(apply(out3$betadraw, c(1,2), effectiveSize))
+mean(apply(out1$betadraw[,,burnin:R], c(1,2), effectiveSize))
+mean(apply(out2$betadraw[,,burnin:R], c(1,2), effectiveSize))
+mean(apply(out3$betadraw[,,burnin:R], c(1,2), effectiveSize))
 
 t1
 t2
@@ -116,11 +121,11 @@ t3
 
 pdf(file="linear_acf_gESS.pdf", height = 6, width = 6)
 
-a1 = acf(out1$betadraw[j,2,],plot=FALSE)
+a1 = acf(out1$betadraw[,,burnin:R][j,2,],plot=FALSE)
 plot(a1$acf~a1$lag,type='h',col="#00000005",lwd=10, xlab = "Lag", ylab = "Autocorrelation")
 
 for(j in 1:50) {
-a1 = acf(out1$betadraw[j,2,],plot=FALSE)
+a1 = acf(out1$betadraw[,,burnin:R][j,2,],plot=FALSE)
 lines(a1$acf~a1$lag,type='h',col="#00000005",lwd=10)
 }
 
@@ -130,11 +135,11 @@ dev.off()
 
 pdf(file="linear_acf_Gibbs.pdf", height = 6, width = 6)
 
-a1 = acf(out2$betadraw[j,2,],plot=FALSE)
+a1 = acf(out2$betadraw[,,burnin:R][j,2,],plot=FALSE)
 plot(a1$acf~a1$lag,type='h',col="#00000005",lwd=10, xlab = "Lag", ylab = "Autocorrelation")
 
 for(j in 1:50) {
-a1 = acf(out2$betadraw[j,2,],plot=FALSE)
+a1 = acf(out2$betadraw[,,burnin:R][j,2,],plot=FALSE)
 lines(a1$acf~a1$lag,type='h',col="#00000005",lwd=10)
 }
 
@@ -144,11 +149,11 @@ dev.off()
 
 pdf(file="linear_acf_ESS.pdf", height = 6, width = 6)
 
-a1 = acf(out3$betadraw[j,2,],plot=FALSE)
+a1 = acf(out3$betadraw[,,burnin:R][j,2,],plot=FALSE)
 plot(a1$acf~a1$lag,type='h',col="#00000005",lwd=10, xlab = "Lag", ylab = "Autocorrelation")
 
 for(j in 1:50) {
-a1 = acf(out3$betadraw[j,2,],plot=FALSE)
+a1 = acf(out3$betadraw[,,burnin:R][j,2,],plot=FALSE)
 lines(a1$acf~a1$lag,type='h',col="#00000005",lwd=10)
 }
 
@@ -160,19 +165,26 @@ dev.off()
 
 
 # check posterior mean and sd
-par(mfrow = c(2,2))
-a = as.vector(apply(out1$betadraw, c(1,2), mean))
-b = as.vector(apply(out2$betadraw, c(1,2), mean))
-d = as.vector(apply(out3$betadraw, c(1,2), mean))
+par(mfrow = c(2,3))
+a = as.vector(apply(out1$betadraw[,,burnin:R], c(1,2), mean))
+b = as.vector(apply(out2$betadraw[,,burnin:R], c(1,2), mean))
+d = as.vector(apply(out3$betadraw[,,burnin:R], c(1,2), mean))
+e = as.vector(apply(out4$betadraw[,,burnin:R], c(1,2), mean))
 plot(b,a, ylab="GESS", xlab="Gibbs", main = "Posterior mean")
 abline(0,1,col="red",lwd = 2)
 plot(b,d, ylab="ESS", xlab="Gibbs", main = "Posterior mean")
 abline(0,1,col="red",lwd = 2)
+plot(b,e, ylab="Gibbs2", xlab="Gibbs", main = "Posterior mean")
+abline(0,1,col="red",lwd = 2)
 
-a = as.vector(apply(out1$betadraw, c(1,2), sd))
-b = as.vector(apply(out2$betadraw, c(1,2), sd))
-d = as.vector(apply(out3$betadraw, c(1,2), sd))
+
+a = as.vector(apply(out1$betadraw[,,burnin:R], c(1,2), sd))
+b = as.vector(apply(out2$betadraw[,,burnin:R], c(1,2), sd))
+d = as.vector(apply(out3$betadraw[,,burnin:R], c(1,2), sd))
+e = as.vector(apply(out4$betadraw[,,burnin:R], c(1,2), sd))
 plot(b,a, ylab="GESS", xlab="Gibbs", main = "Posterior std")
 abline(0,1,col="red",lwd = 2)
 plot(b,d, ylab="ESS", xlab="Gibbs", main = "Posterior std")
+abline(0,1,col="red",lwd = 2)
+plot(b,e, ylab="Gibbs2", xlab="Gibbs", main = "Posterior std")
 abline(0,1,col="red",lwd = 2)
